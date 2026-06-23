cf53ec897fe7d87bfa6ed20a9ab1654630200ce0
noah <bokenf254@gmail.com>
2026-06-23 10:11:12 +0300

    Refactored project structure: moved files to Apache web root

---
+++ Week4/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Page</title>
+</head>
+<body>
+    <h2>Login</h2>
+    <form action="login.php" method="POST">
+        <label>Username:</label>
+        <input type="text" name="username" required>
+        <br>
+        <label>Password:</label>
+        <input type="password" name="password" required>
+        <br>
+        <button type="submit" name="login_btn">Login</button>
+    </form>
+</body>
+</html>
